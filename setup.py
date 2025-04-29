a819e91e168e3202b8985182f1fb52ac6641131f
Jake Searcy <jsearcy1@gmail.com>
2025-04-29 16:11:54 +0000

    add missing py_modules

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup
 
 setup(
     name='ADT',
-    version='0.1.0',
-    py_modules=['listABC', 'ADTexceptions'],
+    version='0.1.1',
+    py_modules=['ADTexceptions',
+                'ADTiterator',
+                'ADTtypemap',
+                'arraystack',
+                'dynamicarray',
+                'listABC',
+                'stackABC']
+,
     install_requires=[],
 )
