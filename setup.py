0a1497f981ba74dc6b88c1cb38fe897a25a57eb4
jsearcy1 <jsearcy1@gmail.com>
2025-05-09 13:04:52 -0700

    add deque to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                 'dynamicarray',
                 'listABC',
                 'stackABC',
-                'dequeueABC',
+                'dequeABC',
                 'arrayqueue',
                 'boundedarraystackint',
                 'boundedstackintABC',
